7675651aad24b2a7d7a0fa868f909bdab684f171
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-18 20:48:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,11 @@ def load_library(path)
 
   emoticon_hash = {'get_meaning' => {}}
   emoticon_hash = {'get_emoticon' => {}}
-
+  emoticons.each do |english_word. emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticocn_set.first] = english_word.last
+  end
+  emoticon_hash
 end
 
 def get_japanese_emoticon
