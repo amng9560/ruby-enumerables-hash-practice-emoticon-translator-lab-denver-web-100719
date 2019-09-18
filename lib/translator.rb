3094597cf840052828d4060b84e605405a51bab3
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-18 20:54:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ require "yaml"
 def load_library(emoticon_file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
   # code goes here
+  emoticon_lib = {'get_meaning' => {}, 'get_emoticon' => {}}
 end
 
 def get_japanese_emoticon
